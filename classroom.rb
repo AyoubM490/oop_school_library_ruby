920b096c0e7e5d8840d8a9cf96f851f47d28077b
ayubch <ayoub.chahir96@gmail.com>
2022-06-15 21:21:04 +0100

    add classroom class

---
+++ classroom.rb
@@ -0,0 +1,17 @@
+$LOAD_PATH << '.'
+require 'student'
+
+class Classroom
+  attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student) unless @students.include?(student)
+    student.classroom = self
+  end
+end
