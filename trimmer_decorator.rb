95a39b289c6224b17bd8bd653f2b8782236c3b3e
ayubch <ayoub.chahir96@gmail.com>
2022-06-15 14:38:24 +0100

    add trimmer decorator class

---
+++ trimmer_decorator.rb
@@ -0,0 +1,8 @@
+$LOAD_PATH << '.'
+require 'base_decorator'
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0...10] : @nameable.correct_name
+  end
+end
